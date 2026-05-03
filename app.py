beb595b18fee3749892ef4ac26ab8f73bca4b1c2
Carlos Dev <carlosmpr.ing.sistemas@gmail.com>
2026-05-02 21:06:21 -0400

    Se crea la funcion para insertar un producto

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
     price = db.Column(db.Float, nullable=False)
-    stock = db.Column(db.Integer, default=0)
+    stock = db.Column(db.Integer, nullable=False, default=0)
 
         
     def __repr__(self):
@@ -25,6 +25,22 @@ def init_database():
         db.create_all()
         print("✨ Database, product table created successfuly!")
         
+def insert_product():
+    with app.app_context():
+        name = input("Nombre del producto: ").strip()
+        price = input("Precio: ").strip()
+        stock = input("Stock (Enter para valor por defecto 0): ").strip()
+
+
+        if name and price and stock:
+            product = Product(name=name, price=float(price), stock=int(stock))
+            db.session.add(product)
+            db.session.commit()
+            print("✍️ Producto Insertado Exitosamente!")
+                    
+        product = Product(name=name, price=float(price))
+        print("✨ Producto insertado con valor por defecto (0) exitosamente!")
 
 if __name__ == "__main__":
     init_database()
+    insert_product()
